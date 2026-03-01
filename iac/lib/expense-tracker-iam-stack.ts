a4165ded4db80b0e63b274ed13cc2f968752b04e
Niketan1604 <niketanswami91@gmail.com>
2026-03-01 20:47:27 +0530

    updated policy to support sam deploy

--- iac/lib/expense-tracker-iam-stack.ts
+++ iac/lib/expense-tracker-iam-stack.ts
@@ -345,7 +345,10 @@ export class ExpenseTrackerIamStack extends Stack {
         's3:GetBucketAcl',
         's3:PutBucketAcl',
         's3:PutBucketPublicAccessBlock',
-        's3:GetBucketPublicAccessBlock'
+        's3:GetBucketPublicAccessBlock',
+        's3:GetObject',
+        's3:PutObject',
+        's3:DeleteObject'
       ],
       resources: [
         `arn:aws:s3:::aws-sam-cli-managed-default-samclisourcebucket-*`,
